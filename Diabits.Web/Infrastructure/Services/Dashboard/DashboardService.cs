aaefcf2138d43fe756ce910333dd5d6f060cb8fd
siljavind <silja.vj@gmail.com>
2026-05-23 15:04:38 +0200

    mobile optimization dashboard data fix todos

--- Diabits.Web/Infrastructure/Services/Dashboard/DashboardService.cs
+++ Diabits.Web/Infrastructure/Services/Dashboard/DashboardService.cs
@@ -1,5 +1,6 @@
 using Diabits.Web.Infrastructure.Api;
 using Diabits.Web.Models;
+using System.Globalization;
 
 namespace Diabits.Web.Infrastructure.Services.Dashboard;
 
@@ -8,7 +9,7 @@ public class DashboardService(ApiClient apiClient)
     public async Task<TimelineChartResponse> GetTimelineAsync(DateTime date)
     {
         //TODO formatting date?
-        var dateStr = date.ToString("yyyy-MM-ddTHH:mm:ss");
+        var dateStr = date.ToString("yyyy-MM-ddTHH:mm:ss",  CultureInfo.InvariantCulture);
 
         return await apiClient.GetAsync<TimelineChartResponse>($"Dashboard/timeline?date={dateStr}")
             ?? throw new HttpRequestException("No data returned");
